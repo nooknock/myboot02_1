d0fb5767613518d293d084dd4194b0d93d92947d
nooknock <wooh2013@gmail.com>
2023-04-19 18:10:33 +0900

    0419_2

--- src/main/java/com/myboot02/DemoController.java
+++ src/main/java/com/myboot02/DemoController.java
@@ -15,10 +15,10 @@ public class DemoController {
 		return "hell";
 	}
 	
-	@RequestMapping("/hello.do")
+	@RequestMapping("/hello.do") //타임리프는 기본 cache 설정이 true 이므로 개발시에는 spring.thymeleaf.cache를 false를 설정하여 두는게 수정 사항이 바로 반영되므로 편리합니다.
 	public String hello(Model model) {
 		model.addAttribute("dasdw","ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ");
-		return "th/hello";
+		return "thymeleaf/hello";
 	}
 	
 	@RequestMapping("/hi.do")
